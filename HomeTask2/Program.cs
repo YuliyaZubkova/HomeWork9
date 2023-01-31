59751f89cec8b5c6ff071624664844b9635e92f5
Yuliya <i@yzubkova.ru>
2023-01-31 16:06:45 +0300

    Программа выводит сумму цифр во введенном числе с помощью рекурсии

--- HomeTask2/Program.cs
+++ HomeTask2/Program.cs
@@ -5,3 +5,43 @@ M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
 
+Console.Clear();
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else 
+        {
+            Console.WriteLine("Введите корректное число.");
+        }
+    }
+    return result;
+}
+
+string GetValues(int n, int m)
+    {
+        if (n == m)
+        {
+            int count = m;
+            return n.ToString();
+        }
+        return GetValues(n-1, m) + $", {count}";
+
+    }
+
+//int n = GetNumber("Введите число N больше M: ");
+int m = GetNumber("Введите число M больше 1: ");
+int n = GetNumber("Введите число N больше M: ");
+
+GetValues(n, m);
+Console.WriteLine(GetValues(n, m));
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая будет принимать на вход число 
+и возвращать сумму его цифр. Например, 
+453 -> 12;
+*/
+
+// 1. Функция получения числа.
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректное число.");
+        }
+    }
+    return result;
+}
+// 2. Функция вычисления суммы цифр числа. 
+int GetSum(int number)
+{
+    int result = 0;
+    if (number < 10)
+    {
+        result = number;
+    }
+    else 
+    {
+        result = GetSum(number/10) + number%10;
+    }
+    return result;
+}
+
+int number = GetNumber("Введите число N больше 1: ");
+Console.WriteLine(GetSum(number));
