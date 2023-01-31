9e0202158ea4af6acd2560163fd06586da459005
Yuliya <i@yzubkova.ru>
2023-01-31 14:50:51 +0300

    Программа выводит чисола от 1 до N с помощью рекурсии

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -3,3 +3,40 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+
+Console.Clear();
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else 
+        {
+            Console.WriteLine("Введите корректное число.");
+        }
+    }
+    return result;
+}
+
+string GetValues(int n)
+    {
+        if (n == 1)
+        {
+            return n.ToString();
+        }
+        return GetValues(n-1) + $", {n}";
+    }
+
+int n = GetNumber("Введите число N больше 1: ");
+
+GetValues(n);
+Console.WriteLine(GetValues(n));
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+
+Console.Clear();
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else 
+        {
+            Console.WriteLine("Введите корректное число.");
+        }
+    }
+    return result;
+}
+
+string GetValues(int n)
+    {
+        if (n == 1)
+        {
+            return n.ToString();
+        }
+        return GetValues(n-1) + $", {n}";
+    }
+
+int n = GetNumber("Введите число N больше 1: ");
+
+GetValues(n);
+Console.WriteLine(GetValues(n));
